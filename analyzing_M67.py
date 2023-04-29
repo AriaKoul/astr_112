372c2db0afc9fe0bc50cdfdabf49486353577598
Aria Koul <ariakoul@gmail.com>
2023-04-29 13:03:26 -0700

    Update analyzing_M67.py

--- analyzing_M67.py
+++ analyzing_M67.py
@@ -4,7 +4,7 @@ DESCRIPTION:
 
 """
 
-### IMPORTS
+### IMPORTS ###
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd 
@@ -17,11 +17,14 @@ from astroquery.gaia import Gaia
 from astropy.coordinates import SkyCoord
 from astroquery.gaia import Gaia
 
-### GETTING THE DATA FROM GAIA
+
+
+### GETTING THE DATA FROM GAIA ###
 query_result = pd.read_csv('M67_results.csv') # Outputted 5546 stars 
 
 
-### CREATING A HISTOGRAM FOR THE PARALLAXES ###
+
+'''### CREATING A HISTOGRAM FOR THE PARALLAXES ###
 parallax_signal = query_result['parallax']
 max_parallax = np.max(parallax_signal) # determines what the largest parallax value is in order to define the bins
 print(max_parallax) # found to be 22.04
@@ -34,17 +37,37 @@ plt.ylabel('Stars')
 
 plt.title('Parallax Histogram')
 
-plt.show()
+plt.show()'''
+
+# Found that most of the stars have a parallax between 1.00 and 1.25 mas
 
 
+
+### FINDING DISTANCE OF M67 CLUSTER USING PARALLAX ###
+
+# All parallax values below are in arcseconds and all distance values are in parsecs.
+'''parallax_lower = 0.001
+distance_lower = 1/parallax_lower
+parallax_upper = 0.00125
+distance_upper = 1/parallax_upper
+
+avg_parallax = 0.00113
+approx_dist = 1/avg_parallax
+'''
 
 
 ### ADJUSTING THE DATA ###
+
 # Some rows had invalid BP - RP values. Below, I filtered the data to get only the
 # stars with valid BP - RP values.
-
 query_result_filtered = query_result.dropna(subset=['bp_rp'])
 
+# In making a Color Magnitude Diagram, we only want the stars that have a parallax
+# value within the range found in the histogram. That is, we want stars that have
+# a parallax between 1.00 mas and 1.25 mas only.
+query_result_filtered.drop(query_result_filtered[query_result_filtered['parallax'] > 1.25].index, inplace=True)
+query_result_filtered.drop(query_result_filtered[query_result_filtered['parallax'] < 1.00].index, inplace=True)
 
+print(query_result_filtered)
 
 
