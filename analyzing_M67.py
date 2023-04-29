f7673793e873f27d496e94e79b0ed49e2a996a8b
Aria Koul <ariakoul@gmail.com>
2023-04-29 12:10:55 -0700

    Creating a parallax histogram

--- analyzing_M67.py
+++ analyzing_M67.py
@@ -20,8 +20,30 @@ from astroquery.gaia import Gaia
 ### GETTING THE DATA FROM GAIA
 query_result = pd.read_csv('M67_results.csv') # Outputted 5546 stars 
 
-# Some rows had invalid effective temperature values and others had invalid AG
-# extinction values
+
+### CREATING A HISTOGRAM FOR THE PARALLAXES ###
+parallax_signal = query_result['parallax']
+max_parallax = np.max(parallax_signal) # determines what the largest parallax value is in order to define the bins
+print(max_parallax) # found to be 22.04
+
+bins = np.arange(0.5,8,0.25) #define bins with the first one at SNR=10 & going up to 4000 in steps of 50
+plt.hist(parallax_signal,bins)
+
+plt.xlabel('Parallax (mas)')
+plt.ylabel('Stars')
+
+plt.title('Parallax Histogram')
+
+plt.show()
+
+
+
+
+### ADJUSTING THE DATA ###
+# Some rows had invalid BP - RP values. Below, I filtered the data to get only the
+# stars with valid BP - RP values.
+
+query_result_filtered = query_result.dropna(subset=['bp_rp'])
 
 
 
