cc1f030d34401f96cc807fc2cc0a2ca723e8b5a4
Aria Koul <ariakoul@gmail.com>
2023-05-17 22:54:41 -0700

    Started code for Praesepe Cluster

---
+++ analyzing_praesepe.py
@@ -0,0 +1,15 @@
+"""
+DESCRIPTION:
+ 
+
+"""
+
+### IMPORTS ###
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd 
+
+
+
+### GETTING THE DATA FROM GAIA ###
+# query_result = pd.read_csv('M67_data.csv') # Outputted stars 
