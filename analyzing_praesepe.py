4447e06a1f5265a938b3a63628edadf2d2aa9192
Aria Koul <ariakoul@gmail.com>
2023-05-19 16:58:25 -0700

    Plotting initial RA vs. DEC

--- analyzing_praesepe.py
+++ analyzing_praesepe.py
@@ -14,29 +14,29 @@ import pandas as pd
 ### GETTING THE DATA FROM GAIA ###
 query_result = pd.read_csv('praesepe_data.csv') # Outputted 141627 stars (using radius = 4 degrees, parallax >= 0.5)
 
-
+'''
 ### CREATING A HISTOGRAM FOR THE PARALLAXES ###
 parallax_signal = query_result['parallax']
 max_parallax = np.max(parallax_signal) # determines what the largest parallax value is in order to define the bins
-# print(max_parallax) # max parallax was 92.22 mas 
+print(max_parallax) # max parallax was 92.22 mas 
 
 # Below is the code for the entire parallax histogram
-# bins = np.arange(0, 8, 0.05) 
-# plt.hist(parallax_signal,bins)
-# plt.xlabel('Parallax (mas)')
-# plt.ylabel('Stars')
-# plt.title('Parallax Histogram')
-# plt.show()
+bins = np.arange(0, 8, 0.05) 
+plt.hist(parallax_signal,bins)
+plt.xlabel('Parallax (mas)')
+plt.ylabel('Stars')
+plt.title('Parallax Histogram')
+plt.show()
 
 # # Below is the code for the parallax histogram zoomed into the parallaxes that the Praesepe Cluster might correspond to
-# bins_zoomed = np.arange(5, 6, 0.02) # arrangement for zoomed in histogram
-# plt.hist(parallax_signal,bins_zoomed)
-# plt.xlabel('Parallax (mas)')
-# plt.ylabel('Stars')
-# plt.title('Zoomed in Parallax Histogram (between 5 and 6 mas)')
-# plt.show()
+bins_zoomed = np.arange(5, 6, 0.02) # arrangement for zoomed in histogram
+plt.hist(parallax_signal,bins_zoomed)
+plt.xlabel('Parallax (mas)')
+plt.ylabel('Stars')
+plt.title('Zoomed in Parallax Histogram (between 5 and 6 mas)')
+plt.show()
 # Peak of the histogram had a width from 5.28 mas to 5.54 mas: this peak was 13 bins across
-
+'''
 
 
 ### FILTERING THE DATA FOR PARALLAX ###
@@ -49,23 +49,23 @@ query_result_filtered = query_result[(query_result['parallax'] >= 5.28) & (query
 # From 5.28 to 5.64: 1105 stars
 
 
-
+'''
 ### PLOTTING THE PROPER MOTION ###
 # In this section, we want to plot proper motion in RA versus proper motion in DEC
-# pm_RA = query_result_filtered['pmra']
-# pm_DEC = query_result_filtered['pmdec']
+pm_RA = query_result_filtered['pmra']
+pm_DEC = query_result_filtered['pmdec']
 
-# fig, ax = plt.subplots(figsize=(6,6), dpi=100)
+fig, ax = plt.subplots(figsize=(6,6), dpi=100)
 
-# ax.scatter(pm_DEC,pm_RA, s=0.3)
+ax.scatter(pm_DEC,pm_RA, s=0.3)
 
-# ax.set_ylabel('Proper Motion in Right Ascension (RA)')
-# ax.set_xlabel('Proper Motion in Declination (DEC)')
+ax.set_ylabel('Proper Motion in Right Ascension (RA)')
+ax.set_xlabel('Proper Motion in Declination (DEC)')
 
-# plt.title('Proper Motion in RA vs. Proper Motion in DEC')
+plt.title('Proper Motion in RA vs. Proper Motion in DEC')
 
-# plt.show()
-
+plt.show()
+'''
 
 
 ### FILTERING THE DATA FOR PROPER MOTION AND PLOTTING PROPER MOTION AGAIN ###
@@ -73,10 +73,12 @@ query_result_filtered = query_result[(query_result['parallax'] >= 5.28) & (query
 query_result_filtered_2 = query_result_filtered[(query_result_filtered['pmdec'] >= -18.0) & (query_result_filtered['pmdec'] <= -7.5)
                                                 & (query_result_filtered['pmra'] >= -45.5) & (query_result_filtered['pmra'] <= -27.0)]
 
+
+'''
 pm_RA_filtered = query_result_filtered_2['pmra']
 pm_DEC_filtered = query_result_filtered_2['pmdec']
 
-fig, ax = plt.subplots(figsize=(6,6), dpi=100)
+fig, ax = plt.subplots(figsize=(5,5), dpi=100)
 
 ax.scatter(pm_DEC_filtered,pm_RA_filtered, s=0.3)
 
@@ -84,5 +86,22 @@ ax.set_ylabel('Proper Motion in Right Ascension (RA)')
 ax.set_xlabel('Proper Motion in Declination (DEC)')
 
 plt.title('Proper Motion in RA vs. Proper Motion in DEC')
+
+plt.show()
+'''
+
+
+### PLOTTING RA vs. DEC FOR INITIAL QUERY (UNFILTERED) ###
+
+RA = query_result['ra']
+DEC = query_result['dec']
+
+fig, ax = plt.subplots(figsize=(5,5), dpi=100)
+ax.scatter(DEC, RA, s = 0.3)
+
+ax.set_ylabel('Right Ascension')
+ax.set_xlabel('Declination')
+
+plt.title('Right Ascension vs. Declination')
 
 plt.show()
