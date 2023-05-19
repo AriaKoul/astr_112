751ac97bf56db22ccfd3c3e03b9bbd2d686e10cd
Aria Koul <ariakoul@gmail.com>
2023-05-19 16:43:52 -0700

    Plotting the proper motion

--- analyzing_praesepe.py
+++ analyzing_praesepe.py
@@ -29,22 +29,60 @@ max_parallax = np.max(parallax_signal) # determines what the largest parallax va
 # plt.show()
 
 # # Below is the code for the parallax histogram zoomed into the parallaxes that the Praesepe Cluster might correspond to
-bins_zoomed = np.arange(5, 6, 0.02) # arrangement for zoomed in histogram
-plt.hist(parallax_signal,bins_zoomed)
-plt.xlabel('Parallax (mas)')
-plt.ylabel('Stars')
-plt.title('Zoomed in Parallax Histogram (between 5 and 6 mas)')
-plt.show()
+# bins_zoomed = np.arange(5, 6, 0.02) # arrangement for zoomed in histogram
+# plt.hist(parallax_signal,bins_zoomed)
+# plt.xlabel('Parallax (mas)')
+# plt.ylabel('Stars')
+# plt.title('Zoomed in Parallax Histogram (between 5 and 6 mas)')
+# plt.show()
 # Peak of the histogram had a width from 5.28 mas to 5.54 mas: this peak was 13 bins across
 
 
 
-### FILTERING THE DATA ###
+### FILTERING THE DATA FOR PARALLAX ###
 
 # We want to filter the stars so that the only stars left in our data set are those that correspond to
 # the parallax range we found. So, we want stars that have a parallax between 5.28 mas and 5.54 mas.
-query_result = query_result[(query_result['parallax'] >= 5.28) & (query_result['parallax'] <= 5.64)]
-print(query_result)
+query_result_filtered = query_result[(query_result['parallax'] >= 5.28) & (query_result['parallax'] <= 5.64)]
 
 # From 5.24 to 5.54: 1041 stars
 # From 5.28 to 5.64: 1105 stars
+
+
+
+### PLOTTING THE PROPER MOTION ###
+# In this section, we want to plot proper motion in RA versus proper motion in DEC
+# pm_RA = query_result_filtered['pmra']
+# pm_DEC = query_result_filtered['pmdec']
+
+# fig, ax = plt.subplots(figsize=(6,6), dpi=100)
+
+# ax.scatter(pm_DEC,pm_RA, s=0.3)
+
+# ax.set_ylabel('Proper Motion in Right Ascension (RA)')
+# ax.set_xlabel('Proper Motion in Declination (DEC)')
+
+# plt.title('Proper Motion in RA vs. Proper Motion in DEC')
+
+# plt.show()
+
+
+
+### FILTERING THE DATA FOR PROPER MOTION AND PLOTTING PROPER MOTION AGAIN ###
+
+query_result_filtered_2 = query_result_filtered[(query_result_filtered['pmdec'] >= -18.0) & (query_result_filtered['pmdec'] <= -7.5)
+                                                & (query_result_filtered['pmra'] >= -45.5) & (query_result_filtered['pmra'] <= -27.0)]
+
+pm_RA_filtered = query_result_filtered_2['pmra']
+pm_DEC_filtered = query_result_filtered_2['pmdec']
+
+fig, ax = plt.subplots(figsize=(6,6), dpi=100)
+
+ax.scatter(pm_DEC_filtered,pm_RA_filtered, s=0.3)
+
+ax.set_ylabel('Proper Motion in Right Ascension (RA)')
+ax.set_xlabel('Proper Motion in Declination (DEC)')
+
+plt.title('Proper Motion in RA vs. Proper Motion in DEC')
+
+plt.show()
