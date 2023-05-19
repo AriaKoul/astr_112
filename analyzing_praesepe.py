4b337a0857339d3fbf0e3aa02857471a4f97613f
Aria Koul <ariakoul@gmail.com>
2023-05-19 11:45:21 -0700

    Added code to create parallax histogram

--- analyzing_praesepe.py
+++ analyzing_praesepe.py
@@ -13,4 +13,20 @@ import pandas as pd
 
 ### GETTING THE DATA FROM GAIA ###
 query_result = pd.read_csv('praesepe_data.csv') # Outputted 141627 stars (radius = 4 degrees)
-print('hello world')
+
+
+### CREATING A HISTOGRAM FOR THE PARALLAXES ###
+parallax_signal = query_result['parallax']
+max_parallax = np.max(parallax_signal) # determines what the largest parallax value is in order to define the bins
+print(max_parallax) 
+
+bins = np.arange(0.5,10, 0.10) 
+plt.hist(parallax_signal,bins)
+
+plt.xlabel('Parallax (mas)')
+plt.ylabel('Stars')
+
+plt.title('Parallax Histogram')
+
+plt.show()
+# Peak of the histogram had a width from 1.10 mas to 1.20 mas
