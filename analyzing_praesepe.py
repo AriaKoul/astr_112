5acaca8be8a5be6d13f63a55f7e7469e6eb0603c
Aria Koul <ariakoul@gmail.com>
2023-05-18 22:40:26 -0700

    Update analyzing_praesepe.py

--- analyzing_praesepe.py
+++ analyzing_praesepe.py
@@ -12,5 +12,5 @@ import pandas as pd
 
 
 ### GETTING THE DATA FROM GAIA ###
-query_result = pd.read_csv('praesepe_data.csv') # Outputted stars 
+query_result = pd.read_csv('praesepe_data.csv') # Outputted 141627 stars (radius = 4 degrees)
 print('hello world')
