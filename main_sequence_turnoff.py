7f02b65f897e9f4f3012cc87d18e45fc9bcd48b7
Aria Koul <ariakoul@gmail.com>
2023-06-04 22:07:44 -0700

    Adjusted files/their initial queries

--- main_sequence_turnoff.py
+++ main_sequence_turnoff.py
@@ -8,9 +8,9 @@ import pandas as pd
 
 ### PLOTTING COLOR MAGNITUDE DIAGRAM FOR ALL FOUR CLUSTERS ###
 
-csv_files = ['M67_data.csv', 'M44_data.csv', 'alpha_per_data.csv']
+csv_files = ['alpha_per_data.csv', 'NGC6774_data.csv', 'M44_data.csv']
 
-
+print(pd.read_csv('NGC6774_data.csv'))
 def color_magnitude_diagram(csv_files):
     for file in csv_files:
         query_result = pd.read_csv(file)
@@ -31,14 +31,16 @@ def color_magnitude_diagram(csv_files):
         abs_Gmag = app_Gmag - 5*np.log10(d/10)
 
         # Creating the Color Magnitude Diagram plot 
-        fig, ax = plt.subplots(figsize=(5,5), dpi=100)
-        ax.scatter(bp_rp,abs_Gmag, s=0.7, label = file)
+        # fig, ax = plt.subplots(figsize=(5,5), dpi=100)
+        # ax.scatter(bp_rp,abs_Gmag, label = file)
+
+        plt.scatter(bp_rp, abs_Gmag, s = 0.5, label = file)
 
     # The y axis is reversed because smaller magnitude values mean brighter stars,and the convention is to put brighter stars at the top
-    ax.set_ylim(16, -4)
+    plt.ylim(16, -4)
 
-    ax.set_ylabel(r'Absolute magnitude [Gaia G-band]')
-    ax.set_xlabel('B-R color [Gaia Bp & Rp bands]')
+    plt.ylabel(r'Absolute magnitude [Gaia G-band]')
+    plt.xlabel('B-R color [Gaia Bp & Rp bands]')
 
     plt.title('Color Magnitude Diagram of Stars')
 
@@ -47,4 +49,4 @@ def color_magnitude_diagram(csv_files):
     plt.show()
 
 
-print(color_magnitude_diagram(csv_files))
+# print(color_magnitude_diagram(csv_files))
